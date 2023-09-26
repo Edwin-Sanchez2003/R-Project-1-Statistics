d80eaf2f2257f38227d3478c7e456bb9e7251e2b
Edwin Sanchez <edwsanch@iu.edu>
2023-09-25 23:26:39 -0400

    finished R project file

--- src/project1.R
+++ src/project1.R
@@ -20,88 +20,151 @@ p_help(psych, web = F)  # Opens help in R Viewer
 setwd("S:\\Classes\\Introduction to Statistics\\Project1\\")
 
 # set this to be the path to where all of our data lives
-BASE_PATH <- ".\\data\\pre_proc_data\\train\\"
+BASE_PATH <- ".\\data\\pre_proc_data\\"
 
 # get data for images in the train set & take a sneak peek in console
-img_data_train <- import(paste(BASE_PATH, "image_data.csv", sep=""))
+img_data_train <- import(paste(BASE_PATH, "train\\image_data.csv", sep=""))
 head(img_data_train)
 
 # get data for objects in the train set & take a sneak peek in console
-obj_data_train <- import(paste(BASE_PATH, "object_data.csv", sep=""))
+obj_data_train <- import(paste(BASE_PATH, "train\\object_data.csv", sep=""))
 head(obj_data_train)
 
+# get data for images in the test set & take a sneak peek in console
+img_data_test <- import(paste(BASE_PATH, "test\\image_data.csv", sep=""))
+head(img_data_test)
 
+# get data for objects in the test set & take a sneak peek in console
+obj_data_test <- import(paste(BASE_PATH, "test\\object_data.csv", sep=""))
+head(obj_data_test)
 
-# DATA VIEWER ##############################################
-# opens up a spreadsheet viewer in r to see our data
-?View
-View(img_data_train)
-View(obj_data_train)
+# get data for images in the validation set & take a sneak peek in console
+img_data_validation <- import(paste(BASE_PATH, "validation\\image_data.csv", sep=""))
+head(img_data_validation)
 
+# get data for objects in the train set & take a sneak peek in console
+obj_data_validation <- import(paste(BASE_PATH, "validation\\object_data.csv", sep=""))
+head(obj_data_validation)
 
-
-# FIRST TOUCH ON DATA ######################################
-# take a quick look at data to get an idea of what's going on
-# try to identify interesting things
-
-## Image Data
-# describe each quantitative variable - first look at data distr.
-describe(img_data_train)
-
-# Object Count
+## OBJECT COUNT IN IMAGES ##################################
 describe(img_data_train$object_count) # get general details
+describe(img_data_validation$object_count) # get general details
+describe(img_data_test$object_count) # get general details
+
+# Put graphs in 3 rows and 1 column
+par(mfrow = c(3, 1))
 
 # create bar plot 
-object_count_images_train <- table(img_data_train$object_count)  # Create table
-barplot(object_count_images_train)              # Bar chart
-plot(object_count_images_train)                 # Default X-Y plot (lines)
+barplot(
+  table(img_data_train$object_count), 
+  main="Train Image Data: Object Count",
+  xlab="Object Count",
+  ylab="Frequency",
+  ylim=c(0, 175))              # Bar chart
+barplot(
+  table(img_data_validation$object_count), 
+  main="Validation Image Data: Object Count",
+  xlab="Object Count",
+  ylab="Frequency",
+  ylim=c(0, 175)
+  )         # Bar chart
+barplot(
+  table(img_data_test$object_count),
+  main="Test Image Data: Object Count",
+  xlab="Object Count",
+  ylab="Frequency",
+  ylim=c(0, 175))               # Bar chart
 
-# get box & whisker plot of object count
-boxplot(img_data_train$object_count)
 
-# Img width vs height
-plot(img_data_train$width, img_data_train$height,
+# get box & whisker plot of object count for each dataset
+boxplot(
+  img_data_train$object_count, 
+  main="Train Image Data: Object Count",
+  xlab="Object Count",
+  ylim= c(0, 35), 
+  horizontal = T)
+boxplot(
+  img_data_validation$object_count,
+  main="Validation Image Data: Object Count",
+  xlab="Object Count",
+  ylim= c(0, 35), 
+  horizontal = T)
+boxplot(
+  img_data_test$object_count,
+  main="Test Image Data: Object Count",
+  xlab="Object Count",
+  ylim= c(0, 35), 
+  horizontal = T)
+
+# Restore graphic parameter
+par(mfrow=c(1, 1))
+
+
+## OBJECT DATA ##############################
+par(mfrow = c(2, 1))
+hist(obj_data_train$x)
+hist(obj_data_train$y)
+par(mfrow = c(1, 1))
+plot(obj_data_train$x, obj_data_train$y,
      col = "#cc0000",  # Hex code for datalab.cc red
      pch = 19,         # Use solid circles for points
-     main = "Train Image Data: Image Width vs. Image Height",
-     xlab = "Image Width",
-     ylab = "Image Height")
+     main = "Train Object Data:Location of Objects",
+     xlab = "X Location",
+     ylab = "Y Location")
 
-plot(img_data_train$object_count, img_data_train$width,
+par(mfrow = c(2, 1))
+hist(obj_data_train$x_norm)
+hist(obj_data_train$y_norm)
+par(mfrow = c(1, 1))
+plot(obj_data_train$x_norm, obj_data_train$y_norm,
+     xlim= c(0.0, 1.0),
+     ylim= c(0.0, 1.0),
      col = "#cc0000",  # Hex code for datalab.cc red
      pch = 19,         # Use solid circles for points
-     main = "Train Image Data: Object Count vs. Image Width",
-     xlab = "Image Object Count",
-     ylab = "Image Width")
+     main = "Train Object Data: Normalized Location of Objects",
+     xlab = "X Location (Normalized)",
+     ylab = "Y Location (Normalized)")
 
-plot(img_data_train$object_count, img_data_train$height,
+## COMBO PLOTS FOR ALL 3 SETS ######################
+
+# plot & compare normalized object locations
+par(mfrow = c(2, 1))
+hist(obj_data_validation$x_norm)
+hist(obj_data_validation$y_norm)
+par(mfrow = c(1, 1))
+par(mfrow = c(2, 1))
+hist(obj_data_test$x_norm)
+hist(obj_data_test$y_norm)
+par(mfrow = c(1, 1))
+
+par(mfrow = c(2, 2))
+plot(obj_data_train$x_norm, obj_data_train$y_norm,
+     xlim= c(0.0, 1.0),
+     ylim= c(0.0, 1.0),
      col = "#cc0000",  # Hex code for datalab.cc red
      pch = 19,         # Use solid circles for points
-     main = "Train Image Data: Object Count vs. Image Height",
-     xlab = "Image Object Count",
-     ylab = "Image Height")
+     main = "Train: Normalized Location of Objects",
+     xlab = "X Location (Normalized)",
+     ylab = "Y Location (Normalized)")
 
-plot(img_data_train$object_count, img_data_train$area,
+plot(obj_data_validation$x_norm, obj_data_validation$y_norm,
+     xlim= c(0.0, 1.0),
+     ylim= c(0.0, 1.0),
      col = "#cc0000",  # Hex code for datalab.cc red
      pch = 19,         # Use solid circles for points
-     main = "Train Image Data: Object Count vs. Image Area",
-     xlab = "Image Object Count",
-     ylab = "Image Area")
+     main = "Train: Normalized Location of Objects",
+     xlab = "X Location (Normalized)",
+     ylab = "Y Location (Normalized)")
 
-# show density of aspect ratios
-describe(img_data_train$aspect_ratio_float)
-aspect_ratio_f <- table(img_data_train$aspect_ratio_float)
-barplot(aspect_ratio_f)              # Bar chart
-plot(aspect_ratio_f)                 # Default X-Y plot (lines)
-hist(img_data_train$aspect_ratio_float)
-
-aspect_ratio <- table(img_data_train$aspect_ratio)
-barplot(aspect_ratio)              # Bar chart
-plot(aspect_ratio)                 # Default X-Y plot (lines)
-
-
-
-
+plot(obj_data_test$x_norm, obj_data_test$y_norm,
+     xlim= c(0.0, 1.0),
+     ylim= c(0.0, 1.0),
+     col = "#cc0000",  # Hex code for datalab.cc red
+     pch = 19,         # Use solid circles for points
+     main = "Train: Normalized Location of Objects",
+     xlab = "X Location (Normalized)",
+     ylab = "Y Location (Normalized)")
+par(mfrow = c(1, 1))
 # CLEAN UP #################################################
 
 # Clear environment
@@ -114,3 +177,4 @@ p_unload(all)  # Remove all add-ons
 cat("\014")  # ctrl+L
 
 # Clear mind :)
+
